6b36a9570abd19f5d987ddd0ccb5980a11d81784
Patrick <52773096+patrick-borje@users.noreply.github.com>
2024-12-11 07:36:01 -0500

    Update cart.routes.js

--- BACKEND/routes/cart.routes.js
+++ BACKEND/routes/cart.routes.js
@@ -7,7 +7,7 @@ import { authenticateUser } from "../Middlewares/AuthMiddleware.js";
 const router = express.Router();
 
 router.post('/add-to-cart',authenticateUser, addToCart);
-router.get('/cartItems/:userId', getCart);
+router.get('/cartItems/:userId',authenticateUser, getCart);
 router.put('/update-item', authenticateUser, updateCart);
 router.delete('/remove-item/:userId/:productId', authenticateUser, removeItem);
 router.post('/checkout/:userId', authenticateUser, checkout);  // New checkout route
